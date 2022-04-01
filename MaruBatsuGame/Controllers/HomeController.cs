5f4dea923091cbc32c721d2a9858a9b54d0525ae
rino3390 <sherikawaii3390@gmail.com>
2022-04-01 18:23:31 +0800

    feat: pass model to view

--- MaruBatsuGame/Controllers/HomeController.cs
+++ MaruBatsuGame/Controllers/HomeController.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using MaruBatsuGame.Models;
 
 namespace MaruBatsuGame.Controllers;
-
 public class HomeController : Controller{
-	public IActionResult Index(){
-		return View();
+	public IActionResult Index(GameModel? gameModel = null){
+		if (gameModel==null){
+			gameModel = new GameModel();
+		}
+		else{
+			gameModel.NextTurn();
+		}
+		return View(gameModel);
 	}
-
 }
